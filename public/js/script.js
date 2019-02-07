499a2a2247b99f370733440c27de00d039691bcf
David Sambuc <dsambuc@free.fr>
2019-02-07 13:06:03 +0100

    BULLES

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,42 @@
 $( "#bulle7").click(function () {
     $('#myModal').modal('show');
 });
+
+$(document).ready(function(){
+    $("#toptip").tooltip({
+        placement:"top",
+
+    });
+});
+
+$('[data-toggle="tooltip"]').each(function(){
+    var options = {
+        html: true
+    };
+
+    if ($(this)[0].hasAttribute('data-type')) {
+        options['template'] =
+            '<div class="tooltip ' + $(this).attr('data-type') + '" role="tooltip">' +
+            '<div class="tooltip-arrow"></div>' +
+            '<div class="tooltip-inner"></div>' +
+            '</div>';
+    }
+
+    $(this).tooltip(options);
+});
+
+$('[data-toggle="modal tooltip"]').each(function(){
+    var options = {
+        html: true
+    };
+
+    if ($(this)[0].hasAttribute('data-type')) {
+        options['template'] =
+            '<div class="tooltip ' + $(this).attr('data-type') + '" role="tooltip">' +
+            '<div class="tooltip-arrow"></div>' +
+            '<div class="tooltip-inner"></div>' +
+            '</div>';
+    }
+
+    $(this).tooltip(options);
+});
